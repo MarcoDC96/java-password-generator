52f9a39f486f5eda2cfa2010f6e7d11925fd5783
MarcoDC96 <marco.decicco@live.it>
2023-11-29 14:26:07 +0100

    Finish.

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,2 +1,34 @@
-package org.lessons.java.security;public class PasswordGenerator {
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci il tuo nome: ");
+        String userName= scan.nextLine();
+
+        System.out.print("Inserisci il tuo cognome: ");
+        String userSurname= scan.nextLine();
+
+        System.out.print("Inserisci il tuo colore preferito: ");
+        String userFavoriteColour= scan.nextLine();
+
+        System.out.print("Inserisci il tuo giorno di nascita: ");
+        int userGiornoNascita= scan.nextInt();
+
+        System.out.print("Inserisci il tuo mese di nascita: ");
+        int userMeseNascita= scan.nextInt();
+
+        System.out.print("Inserisci il tuo anno di nascita: ");
+        int userAnnoNascita= scan.nextInt();
+
+        int sommaGiornoMeseAnnoNascita = userGiornoNascita + userMeseNascita + userAnnoNascita;
+
+        String password = userName + "-" + userSurname + "-" + userFavoriteColour + "-" + sommaGiornoMeseAnnoNascita;
+        System.out.println("La tua password è: " + password);
+
+        scan.close();
+    }
 }
